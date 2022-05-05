44977f2eb820d7c1a4597fc002b84c847e60b248
yap8 <nebozhenko.arseny@gmail.com>
2022-05-05 19:05:28 +0300

    changed color of CartLink

--- src/components/Cart/CartLink.tsx
+++ src/components/Cart/CartLink.tsx
@@ -5,10 +5,10 @@ const cartItemsCount: number = 3;
 const CartLink: React.FC = () => {
   return (
     <div className="flex relative">
-      <div className="flex font-semibold items-center justify-center h-6 w-6 bg-violet-600 text-white rounded-full absolute -top-2 -right-2">
+      <div className="flex font-semibold items-center justify-center h-6 w-6 bg-blue-400 text-white rounded-full absolute -top-2 -right-2">
         {cartItemsCount}
       </div>
-      <AiOutlineShoppingCart className="text-3xl transition hover:text-violet-600" />
+      <AiOutlineShoppingCart className="text-3xl" />
     </div>
   );
 };
