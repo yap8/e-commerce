6a9c05f688dcd0db7a91f8df5e8496a323649e10
yap8 <nebozhenko.arseny@gmail.com>
2022-05-05 16:10:24 +0300

    defined Header structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import Header from './components/Header';
 import Home from './pages/Home';
 
-const App = () => {
+const App: React.FC = () => {
   return (
-    <div className="App">
+    <>
+      <Header />
       <Home />
-    </div>
+    </>
   );
 };
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+const Header: React.FC = () => {
+  return (
+    <header>
+      <div className="container h-16 flex items-center justify-center">
+        <div className="flex-1">LANGUAGE SEARCH</div>
+        <div className="flex-1">LOGO</div>
+        <div className="flex-1">REGISTER SIGNIN CART</div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
