dcb47a9554ddf19a8ace6910c5de7048b29e0b9e
yap8 <nebozhenko.arseny@gmail.com>
2022-05-05 22:05:22 +0300

    finished NavButton

--- src/components/Banners/NavButton.tsx
+++ src/components/Banners/NavButton.tsx
@@ -1,4 +1,5 @@
 import { useSwiper } from 'swiper/react';
+import { AiFillCaretLeft, AiFillCaretRight } from 'react-icons/ai';
 
 interface NavButtonProps {
   prev?: boolean;
@@ -11,12 +12,12 @@ const NavButton: React.FC<NavButtonProps> = ({ prev, next, className }) => {
 
   return (
     <button
-      className={`absolute w-20 h-20 bg-white/80 rounded-full backdrop-blur ${
-        next ? 'left-0' : 'right-0'
+      className={`flex items-center text-2xl text-gray-400 justify-center absolute w-16 h-16 bg-gray-200/80 rounded-full backdrop-blur top-1/2 -translate-y-1/2 z-10 hover:text-gray-500 hover:bg-gray-300/80 transition ${
+        next ? 'right-4' : 'left-4'
       } ${className}`}
       onClick={() => (next ? swiper.slideNext() : swiper.slidePrev())}
     >
-      {next ? '>' : '<'}
+      {next ? <AiFillCaretRight /> : <AiFillCaretLeft />}
     </button>
   );
 };
